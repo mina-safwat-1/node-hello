18559256b98ba4fc15f6fbb848f20893cdeb46bc
mina <minasafwat142@gmail.com>
2025-09-04 10:37:19 +0300

    Editing eslint.config.cjs file

--- application_code/eslint.config.cjs
+++ application_code/eslint.config.cjs
@@ -4,10 +4,16 @@ const js = require("@eslint/js");
 module.exports = [
   js.configs.recommended,
   {
+    languageOptions: {
+      env: {
+        node: true, // Enables ALL Node.js globals (require, module, process, console, etc.)
+        es2021: true, // Enables modern JS globals (Promise, globalThis, etc.)
+      },
+    },
     rules: {
-      semi: "warn",                // warn about missing semicolons
-      quotes: ["warn", "double"],  // warn about single quotes
-      "no-unused-vars": "warn",    // keep this as warning
+      semi: "warn",                // just warn
+      quotes: ["warn", "double"],  // just warn
+      "no-unused-vars": "warn",    // just warn
     },
   },
 ];
